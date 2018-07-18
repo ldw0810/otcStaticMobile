c65e8a32d9541aa98b17b69ce0e12782471f0008
liudewei <3151081884@qq.com>
2018-07-18 12:02:58 +0800

    代码提交

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  .container
+    transition(name="fade" mode="out-in")
+      router-view
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="stylus" scoped>
+  .container {
+    .alert {
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+</style>
