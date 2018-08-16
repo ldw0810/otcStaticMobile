ead8e0d935a4be405b79edf95e4ae283d7eb557d
liudewei <3151081884@qq.com>
2018-08-16 18:38:54 +0800

    分享页面图片修改

--- src/components/trade/adShare.vue
+++ src/components/trade/adShare.vue
@@ -4,11 +4,13 @@
       router-link(to="/myAd" slot="left")
         mt-button(icon="back")
     .wrapper(@click="headerFlag = !headerFlag")
-      .content(:class="{'contentCn': language === 'cn', 'contentEn': language === 'en'}")
+      .content(id="adShareContent")
         qrcode-vue(ref="qrCode" class="qrCode" :value='qrCodeConfig.value' :size='qrCodeConfig.size')
+        .image(ref="image")
+        <!--mt-button(class="popDownload" @click="success") {{$t('public.invite_image_info_text')}}-->
 </template>
 <script type="es6">
-import {Header, Button, Field} from 'mint-ui'
+import {Button, Field, Header} from 'mint-ui'
 import Vue from 'vue'
 import QrcodeVue from 'qrcode.vue'
 import {$getAxiosLanguage} from '../../utils'
@@ -33,6 +35,9 @@ export default {
     language () {
       return $getAxiosLanguage().toLowerCase().indexOf('cn') > -1 ? 'cn' : 'en'
     },
+    shareImg () {
+      return require(`../../assets/images/trade/adShare_${this.language}.png`)
+    },
     id () {
       return this.$route.query.id || ''
     },
@@ -52,8 +57,55 @@ export default {
     }
   },
   methods: {
+    success () {
+      this.$emit('close', 1)
+      this.$emit('success', 1)
+    },
+    getClientSize (num) {
+      return +num * configure.qrCode_adShare.multiple * document.body.clientWidth / 375
+    },
+    convertCanvasToImage (canvas) {
+      let image = new Image()
+      image.src = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+      return image.src
+    },
+    createImage () {
+      let qrCodeImg = this.convertCanvasToImage(this.$refs.qrCode.$el.children[0])
+      let imgArr = [this.shareImg, qrCodeImg]
+      let c = document.createElement('canvas')
+      let ctx = c.getContext('2d')
+      c.width = document.body.clientWidth * configure.qrCode_adShare.multiple
+      c.height = document.body.clientWidth * configure.qrCode_adShare.multiple * 16 / 9
+      ctx.rect(0, 0, c.width, c.height)
+      ctx.fillStyle = '#ccc'
+      ctx.fill()
+      const drawing = (number) => {
+        let index = +number || 0
+        if (index < imgArr.length) {
+          let img = new Image()
+          img.src = imgArr[index]
+          img.onload = () => {
+            if (index === 1) {
+              ctx.drawImage(img, this.getClientSize(configure.qrCode_adShare.left), this.getClientSize(configure.qrCode_adShare.top), this.getClientSize(configure.qrCode_adShare.size), this.getClientSize(configure.qrCode_adShare.size))
+              drawing(++index)
+            } else {
+              ctx.drawImage(img, 0, 0, c.width, c.height)
+              drawing(++index)
+            }
+          }
+        } else {
+          this.imageData = c.toDataURL('image/png').replace('image/png', 'image/octet-stream')
+          this.$refs.image.innerHTML = '<img width=\'' + this.clientHeight * 0.45 +
+              '\' height=\'' + this.clientHeight * 0.8 + '\' src=\'' + this.imageData + '\'>'
+          document.getElementById('adShareContent').style.alignItems = 'center'
+          document.getElementById('adShareContent').style.justifyContent = 'center'
+        }
+      }
+      drawing()
+    },
     init () {
       this.$loading.close()
+      this.createImage()
       this.headerFlag = false
     }
   },
@@ -90,23 +142,25 @@ export default {
       border-top 1px solid #EEEEEE
       border-bottom 1px solid #EEEEEE
       .qrCode {
-        margin-top 59.5vh
-        display flex
-        align-items center
-        justify-content center
+        position: absolute;
+        top: -100vh;
+        left: -100vw;
+        visibility: hidden;
+      }
+      .image {
+        width 100vw
+        height 100vw * 1334 / 750
+        /deep/ img {
+          object-fit: cover;
+          object-position: 0 0;
+          width: 100%;
+          height: 100%;
+        }
       }
     }
-    .contentCn {
-      background url('../../assets/images/trade/adShare_cn.png')
-      background-repeat: no-repeat;
-      background-position: center center;
-      background-size: cover;
-    }
-    .contentEn {
-      background url('../../assets/images/trade/adShare_en.png')
-      background-repeat: no-repeat;
-      background-position: center center;
-      background-size: cover;
+    .center {
+      align-items center
+      justify-content center
     }
   }
 </style>
