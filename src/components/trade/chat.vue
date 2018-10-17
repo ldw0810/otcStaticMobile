ecdcb91a1de6fd229b4160bbd67290f29c370e82
刘德伟 <3151081884@qq.com>
2018-10-17 13:36:16 +0800

    input输入框在失去焦点时，记录光标位置，重新获得焦点时，返回之前的光标位置(解决打开表情时输入表情的位置展示问题)

--- src/components/trade/chat.vue
+++ src/components/trade/chat.vue
@@ -3,7 +3,7 @@
     ul
       li(v-for="(chat, index) in msgList" :key="index" :class="{'an-move-right': +chat.type === 0, 'an-move-left': +chat.type === 1, 'an-move-center': +chat.type === 9}")
         p(class="time system" v-if="+chat.type === 9")
-          span(v-html="$getDateStr(new Date(chat.time)) + '<br/>' + parseBrow(chat.data)")
+          span(v-html="$getDateStr(new Date(chat.time)) + '<br/>' + parseText(chat.data)")
         div(:class="'main' + (+chat.type === 0 ? ' self': '')" v-else)
           p(class="time" v-if="chat.timeFlag")
             span(v-text="$getDateStr(new Date(chat.time))")
@@ -14,7 +14,7 @@
             span(class='chat-loading' v-show='chat.status === 0')
             span(:title='$t("order.order_chat_send_msg_fail")' class='chat-error' v-show='chat.status === -1') !
           <!-- 文本 -->
-          .text(v-html="parseBrow(chat.data)")
+          .text(v-html="parseText(chat.data)")
           <!-- 图片 -->
           .text(v-if="+chat.type === 3")
             img(:src="chat.data" class="image" :alt="$t('order.order_chat_img')")
@@ -227,12 +227,21 @@ export default {
       this.msg && (this.msgList[this.msgList.length] = this.msg)
       this.getMsg()
     },
+    parseText (text) {
+      return this.parseBrow(this.parseImage(text))
+    },
+    parseImage (text) {
+      if (!text) {
+        return text
+      }
+      return text
+    },
     parseBrow (text, isNoGif) {
       const list = ['微笑', '撇嘴', '色', '发呆', '得意', '流泪', '害羞', '闭嘴', '睡', '大哭', '尴尬', '发怒', '调皮', '呲牙', '惊讶', '难过', '酷', '冷汗', '抓狂', '吐', '偷笑', '愉快', '白眼', '傲慢', '饥饿', '困', '惊恐', '流汗', '憨笑', '大兵', '奋斗', '咒骂', '疑问', '嘘', '晕', '折磨', '衰', '骷髅', '敲打', '再见', '擦汗', '抠鼻', '鼓掌', '糗大了', '坏笑', '左哼哼', '右哼哼', '哈欠', '鄙视', '委屈', '快哭了', '阴险', '亲亲', '吓', '可怜', '菜刀', '西瓜', '啤酒', '篮球', '乒乓', '咖啡', '饭', '猪头', '玫瑰', '凋谢', '示爱', '爱心', '心碎', '蛋糕', '闪电', '炸弹', '刀', '足球', '瓢虫', '便便', '月亮', '太阳', '礼物', '拥抱', '强', '弱', '握手', '胜利', '抱拳', '勾引', '拳头', '差劲', '爱你', 'NO', 'OK', '爱情', '飞吻', '跳跳', '发抖', '怄火', '转圈', '磕头', '回头', '跳绳', '挥手', '激动', '街舞', '献吻', '左太极', '右太极', '嘿哈', '捂脸', '奸笑', '机智', '皱眉', '耶', '红包', '鸡']
       if (!text) {
         return text
       }
-      text = text.replace(/\[[\u4E00-\u9FA5]{1,3}\]/gi, function (word) {
+      text = text.replace(/\[[\u4E00-\u9FA5]{1,3}\]/gi, (word) => {
         let newWord = word.replace(/\[|\]/gi, '')
         let index = list.indexOf(newWord)
         let backgroundPositionX = -index * 24
